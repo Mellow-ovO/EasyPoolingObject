f38ce62ac8245c7e2679a45ed77cf24e5ec56c11
HYJ <982278755@qq.com>
2025-02-19 23:18:59 +0800

    feat: request_k2node

---
+++ Source/EasyPoolingObjectDev/K2Node/RequestPoolableObject/K2Node_RequestPoolableObject.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "K2Node_BaseAsyncTask.h"
+#include "K2Node_RequestPoolableObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EASYPOOLINGOBJECTDEV_API UK2Node_RequestPoolableObject : public UK2Node_BaseAsyncTask
+{
+	GENERATED_BODY()
+};
